bf2a88ab1678979f4faf0a890964614720bff872
A00287845 <148058135+A00287845@users.noreply.github.com>
2024-03-24 23:06:25 +0000

    Initial Android Commit

---
+++ Android/app/src/main/java/com/example/finalandroidmqtt/MqttApplication.java
@@ -0,0 +1,24 @@
+package com.example.finalandroidmqtt;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.example.finalandroidmqtt.util.Mqtt;
+
+public class MqttApplication extends Application {
+    private Mqtt mqttInstance;
+
+    @Override
+    public void onCreate() {
+        Log.d("MqttApplication", "onCreate: ");
+        super.onCreate();
+        mqttInstance = Mqtt.getInstance();
+    }
+
+    public Mqtt getMqtt() {
+        if (mqttInstance == null) {
+            mqttInstance = Mqtt.getInstance();
+        }
+        return mqttInstance;
+    }
+}
---
+++ Android/app/src/main/java/com/example/finalandroidmqtt/util/Mqtt.java
@@ -0,0 +1,94 @@
+package com.example.finalandroidmqtt.util;
+
+import android.content.Context;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.util.ArrayList;
+
+public class Mqtt {
+
+    private static volatile Mqtt instance;
+    private ArrayList<MqttAndroidClient> clients;
+
+    private Mqtt() {
+        clients = new ArrayList<>();
+    }
+
+    public static Mqtt getInstance() {
+        if (instance == null) {
+            synchronized (Mqtt.class) {
+                if (instance == null) {
+                    instance = new Mqtt();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    public void setupBroker(Context appContext, String cliendId, String brokerUri) {
+        MqttAndroidClient mqttAndroidClient = getMqttAndroidClient(appContext, cliendId, brokerUri);
+
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setAutomaticReconnect(true);
+        options.setCleanSession(true);
+        options.setConnectionTimeout(10);
+
+        try {
+            IMqttToken token = mqttAndroidClient.connect(options);
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Log.d("Mqtt setupBroker", "onSuccess: Adding " + mqttAndroidClient.getClientId() + " to connected clients list.");
+                    clients.add(mqttAndroidClient);
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    Log.e("Mqtt setupBroker", "onFailure: ", exception);
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @NonNull
+    private static MqttAndroidClient getMqttAndroidClient(Context appContext, String clientId, String serverUri) {
+        MqttAndroidClient mqttAndroidClient = new MqttAndroidClient(appContext, serverUri, clientId);
+
+        mqttAndroidClient.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable cause) {
+                Log.e("Mqtt getMqttAndroidClient", "connectionLost: client: " + mqttAndroidClient.getClientId(), cause);
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message){
+                Log.d("Mqtt getMqttAndroidClient", "messageArrived: topic: " + topic + " message " + message);
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken token) {
+                Log.d("Mqtt getMqttAndroidClient", "deliveryComplete: token: " + token);
+            }
+        });
+        return mqttAndroidClient;
+    }
+
+    public ArrayList<MqttAndroidClient> getClients() {
+        return clients;
+    }
+}
