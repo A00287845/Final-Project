b4ce0702a1f6214005e09502ec779d4e216ab797
A00287845 <148058135+A00287845@users.noreply.github.com>
2024-03-24 23:28:10 +0000

    Initial Android Commit
    Added some Mqtt stuff

--- Android/app/src/main/java/com/example/finalandroidmqtt/MqttApplication.java
+++ Android/app/src/main/java/com/example/finalandroidmqtt/MqttApplication.java
@@ -3,16 +3,25 @@ package com.example.finalandroidmqtt;
 import android.app.Application;
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
+
 import com.example.finalandroidmqtt.util.Mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttClient;
+
+import java.util.ArrayList;
 
 public class MqttApplication extends Application {
     private Mqtt mqttInstance;
+
+    private MutableLiveData<ArrayList<MqttClient>> mutableMqttClientList;
 
     @Override
     public void onCreate() {
         Log.d("MqttApplication", "onCreate: ");
         super.onCreate();
         mqttInstance = Mqtt.getInstance();
+        mutableMqttClientList = new MutableLiveData<>();
     }
 
     public Mqtt getMqtt() {
@@ -20,5 +29,22 @@ public class MqttApplication extends Application {
             mqttInstance = Mqtt.getInstance();
         }
         return mqttInstance;
+    }
+
+    public MutableLiveData<ArrayList<MqttClient>> getMutableMqttClientList() {
+        return mutableMqttClientList;
+    }
+
+    public void setMutableMqttClientList(ArrayList<MqttClient> clientList){
+        mutableMqttClientList.setValue(clientList);
+    }
+
+    public void addClientToList(MqttClient client){
+        ArrayList<MqttClient> clientList = mutableMqttClientList.getValue();
+        if (clientList == null){
+            clientList = new ArrayList<>();
+        }
+        clientList.add(client);
+        mutableMqttClientList.setValue(clientList);
     }
 }
